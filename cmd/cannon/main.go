29c1437a238cd69d8fbcb8bab516942cf258eac4
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-15 11:12:04 -0400

    Draw cannon to canvas

--- cmd/cannon/main.go
+++ cmd/cannon/main.go
@@ -4,6 +4,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
 
 	"github.com/gmlewis/rtc/rtc"
 )
@@ -12,8 +15,9 @@ var (
 	posx  = flag.Float64("px", 0, "Projectile position x")
 	posy  = flag.Float64("py", 1, "Projectile position y")
 	posz  = flag.Float64("pz", 0, "Projectile position z")
+	vels  = flag.Float64("vs", 11.25, "Projectile velocity vector scale factor")
 	velx  = flag.Float64("vx", 1, "Projectile velocity vector x")
-	vely  = flag.Float64("vy", 1, "Projectile velocity vector y")
+	vely  = flag.Float64("vy", 1.8, "Projectile velocity vector y")
 	velz  = flag.Float64("vz", 0, "Projectile velocity vector z")
 	gravx = flag.Float64("gx", 0, "Environment gravity vector x")
 	gravy = flag.Float64("gy", -0.1, "Environment gravity vector y")
@@ -21,6 +25,8 @@ var (
 	windx = flag.Float64("wx", -0.01, "Environment wind vector x")
 	windy = flag.Float64("wy", 0, "Environment wind vector y")
 	windz = flag.Float64("wz", 0, "Environment wind vector z")
+
+	ppmFile = flag.String("ppm", "cannon.ppm", "Output PPM file")
 )
 
 type Projectile struct {
@@ -36,16 +42,29 @@ type Environment struct {
 func main() {
 	p := &Projectile{
 		Position: rtc.Point(*posx, *posy, *posz),
-		Velocity: rtc.Vector(*velx, *vely, *velz),
+		Velocity: rtc.Vector(*velx, *vely, *velz).Normalize().MulScalar(*vels),
 	}
 	e := &Environment{
 		Gravity: rtc.Vector(*gravx, *gravy, *gravz),
 		Wind:    rtc.Vector(*windx, *windy, *windz),
 	}
 
+	var maxx, maxy float64
+	var xvals []int
+	var yvals []int
+
 	var ticks int
 	for {
-		fmt.Printf("After tick #%v, position: (%0.2f,%0.2f,%0.2f)\n", ticks, p.Position.X(), p.Position.Y(), p.Position.Z())
+		if p.Position.X() > maxx {
+			maxx = p.Position.X()
+		}
+		if p.Position.Y() > maxy {
+			maxy = p.Position.Y()
+		}
+		xvals = append(xvals, int(math.Floor(0.5+p.Position.X())))
+		yvals = append(yvals, int(math.Floor(0.5+p.Position.Y())))
+
+		fmt.Printf("After tick #%v, position: (%0.2f,%0.2f)\n", ticks, p.Position.X(), p.Position.Y())
 		if p.Position.Y() <= 0 {
 			break
 		}
@@ -53,6 +72,22 @@ func main() {
 		ticks++
 	}
 	fmt.Printf("Projectile hit the ground after %v ticks.\n", ticks)
+
+	if *ppmFile != "" {
+		width := int(math.Floor(1.5 + maxx))
+		height := int(math.Floor(1.5 + maxy))
+		c := rtc.NewCanvas(width, height)
+
+		red := rtc.Color(1, 0, 0)
+		for i, xv := range xvals {
+			c.WritePixel(xv, height-yvals[i]-1, red)
+		}
+
+		ppm := c.ToPPM()
+		if err := ioutil.WriteFile(*ppmFile, []byte(ppm), 0644); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func tick(env *Environment, proj *Projectile) *Projectile {
