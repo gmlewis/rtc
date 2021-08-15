02668a22b7f7a798580a2787d01bf535fd77786c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-15 15:07:57 -0400

    Add M4 Equal method

---
+++ rtc/matrix.go
@@ -0,0 +1,38 @@
+package rtc
+
+import (
+	"github.com/gmlewis/go3d/float64/mat2"
+	"github.com/gmlewis/go3d/float64/mat3"
+)
+
+// M4 is a 4x4 matrix.
+type M4 [4]Tuple
+
+// Get returns a value within the matrix.
+func (m M4) Get(row, col int) float64 {
+	return m[row][col]
+}
+
+// Equal tests if two matrices are equal.
+func (m M4) Equal(other M4) bool {
+	return m[0].Equal(&other[0]) &&
+		m[1].Equal(&other[1]) &&
+		m[2].Equal(&other[2]) &&
+		m[3].Equal(&other[3])
+}
+
+// M3 is a 3x3 matrix.
+type M3 mat3.T
+
+// Equal tests if two matrices are equal.
+func (m M3) Equal(other M3) bool {
+	return true
+}
+
+// M2 is a 2x2 matrix.
+type M2 mat2.T
+
+// Equal tests if two matrices are equal.
+func (m M2) Equal(other M2) bool {
+	return true
+}
---
+++ rtc/matrix_test.go
@@ -0,0 +1,111 @@
+package rtc
+
+import (
+	"math"
+	"testing"
+
+	"github.com/gmlewis/go3d/float64/mat2"
+	"github.com/gmlewis/go3d/float64/mat3"
+	"github.com/gmlewis/go3d/float64/vec2"
+	"github.com/gmlewis/go3d/float64/vec3"
+)
+
+func TestMatrix_Construction4x4(t *testing.T) {
+	m := M4{
+		Tuple{1, 2, 3, 4},
+		Tuple{5.5, 6.5, 7.5, 8.5},
+		Tuple{9, 10, 11, 12},
+		Tuple{13.5, 14.5, 15.5, 16.5},
+	}
+
+	assertValue := func(row, col int, want float64) {
+		got := m.Get(row, col)
+		if math.Abs(got-want) > epsilon {
+			t.Errorf("m[%v,%v] = %v, want %v", row, col, got, want)
+		}
+	}
+
+	assertValue(0, 0, 1)
+	assertValue(0, 3, 4)
+	assertValue(1, 0, 5.5)
+	assertValue(1, 2, 7.5)
+	assertValue(2, 2, 11)
+	assertValue(3, 0, 13.5)
+	assertValue(3, 2, 15.5)
+}
+
+func TestMatrix_Construction2x2(t *testing.T) {
+	m := mat2.T{
+		vec2.T{-3, 5},
+		vec2.T{1, -2},
+	}
+
+	assertValue := func(row, col int, want float64) {
+		got := m.Get(row, col) // row-major
+		if math.Abs(got-want) > epsilon {
+			t.Errorf("m[%v,%v] = %v, want %v", row, col, got, want)
+		}
+	}
+
+	assertValue(0, 0, -3)
+	assertValue(0, 1, 5)
+	assertValue(1, 0, 1)
+	assertValue(1, 1, -2)
+}
+
+func TestMatrix_Construction3x3(t *testing.T) {
+	m := mat3.T{
+		vec3.T{-3, 5, 0},
+		vec3.T{1, -2, -7},
+		vec3.T{0, 1, 1},
+	}
+
+	assertValue := func(row, col int, want float64) {
+		got := m.Get(row, col) // row-major
+		if math.Abs(got-want) > epsilon {
+			t.Errorf("m[%v,%v] = %v, want %v", row, col, got, want)
+		}
+	}
+
+	assertValue(0, 0, -3)
+	assertValue(0, 1, 5)
+	assertValue(1, 0, 1)
+	assertValue(1, 1, -2)
+	assertValue(2, 2, 1)
+}
+
+func TestMatrix_Equal4x4(t *testing.T) {
+	m1 := M4{
+		Tuple{1, 2, 3, 4},
+		Tuple{5, 6, 7, 8},
+		Tuple{9, 8, 7, 6},
+		Tuple{5, 4, 3, 2},
+	}
+	m2 := M4{
+		Tuple{1, 2, 3, 4},
+		Tuple{5, 6, 7, 8},
+		Tuple{9, 8, 7, 6},
+		Tuple{5, 4, 3, 2},
+	}
+
+	if !m1.Equal(m2) {
+		t.Errorf("4x4 m1.Equal(m2) = %v, want true", false)
+	}
+	if !m2.Equal(m1) {
+		t.Errorf("4x4 m2.Equal(m1) = %v, want true", false)
+	}
+
+	m3 := M4{
+		Tuple{2, 3, 4, 5},
+		Tuple{6, 7, 8, 9},
+		Tuple{8, 7, 6, 5},
+		Tuple{4, 3, 2, 1},
+	}
+
+	if m1.Equal(m3) {
+		t.Errorf("4x4 m1.Equal(m3) = %v, want false", true)
+	}
+	if m3.Equal(m1) {
+		t.Errorf("4x4 m3.Equal(m1) = %v, want false", true)
+	}
+}
