a9175dc95b45716d014b086bfee44d4a828f56e7
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-14 22:46:28 -0400

    Add WritePixel unit test

--- rtc/canvas_test.go
+++ rtc/canvas_test.go
@@ -31,3 +31,13 @@ func TestNewCanvas(t *testing.T) {
 		}
 	}
 }
+
+func TestCanvas_WritePixel(t *testing.T) {
+	c := NewCanvas(10, 20)
+	red := Color(1, 0, 0)
+	c.WritePixel(2, 3, red)
+	got := c.PixelAt(2, 3)
+	if !got.Equal(red) {
+		t.Errorf("PixelAt(2,3) = %v, want %v", got, red)
+	}
+}
