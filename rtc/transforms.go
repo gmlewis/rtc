b815c7bfcd9ee3f8da4bf695d75b56829d917e3c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-15 21:42:21 -0400

    Implement Scaling

--- rtc/transforms.go
+++ rtc/transforms.go
@@ -15,3 +15,19 @@ func (m M4) Translate(x, y, z float64) M4 {
 	t := Translation(x, y, z)
 	return t.Mult(m)
 }
+
+// Scaling returns a 4x4 scaling matrix.
+func Scaling(x, y, z float64) M4 {
+	return M4{
+		Tuple{x, 0, 0, 0},
+		Tuple{0, y, 0, 0},
+		Tuple{0, 0, z, 0},
+		Tuple{0, 0, 0, 1},
+	}
+}
+
+// Scale scales a 4x4 matrix and returns a new one.
+func (m M4) Scale(x, y, z float64) M4 {
+	t := Scaling(x, y, z)
+	return t.Mult(m)
+}
--- rtc/transforms_test.go
+++ rtc/transforms_test.go
@@ -21,3 +21,29 @@ func TestTranslation(t *testing.T) {
 		t.Errorf("transform.MultTuple(v) = %v, want %v", got, v)
 	}
 }
+
+func TestScaling(t *testing.T) {
+	transform := Scaling(2, 3, 4)
+	p := Point(-4, 6, 8)
+	if got, want := transform.MultTuple(p), Point(-8, 18, 32); got != want {
+		t.Errorf("transform.MultTuple(p) = %v, want %v", got, want)
+	}
+
+	v := Vector(-4, 6, 8)
+	if got, want := transform.MultTuple(v), Vector(-8, 18, 32); !got.Equal(want) {
+		t.Errorf("transform.MultTuple(v) = %v, want %v", got, want)
+	}
+
+	inv := transform.Inverse()
+	if got, want := inv.MultTuple(v), Vector(-2, 2, 2); got != want {
+		t.Errorf("inv.MultTuple(v) = %v, want %v", got, want)
+	}
+}
+
+func TestScaling_Reflection(t *testing.T) {
+	transform := Scaling(-1, 1, 1)
+	p := Point(2, 3, 4)
+	if got, want := transform.MultTuple(p), Point(-2, 3, 4); got != want {
+		t.Errorf("transform.MultTuple(p) = %v, want %v", got, want)
+	}
+}
