ac4ce8e8e0343695d47cfbdfc8a681224268daaa
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-17 15:46:43 -0400

    Add PatternAt

--- rtc/pattern.go
+++ rtc/pattern.go
@@ -4,6 +4,11 @@ package rtc
 type Pattern interface {
 	// LocalPatternAt returns a color at a local point.
 	LocalPatternAt(localPoint Tuple) Tuple
+
+	// Transform returns the object's transform 4x4 matrix.
+	Transform() M4
+	// SetTransform sets the object's transform 4x4 matrix.
+	SetTransform(m M4)
 }
 
 // BasePattern represents the common functionality for all patterns.
@@ -19,4 +24,12 @@ func (s *BasePattern) Transform() M4 {
 // SetTransform sets the object's transform 4x4 matrix.
 func (s *BasePattern) SetTransform(m M4) {
 	s.transform = m
+}
+
+// PatternAt returns the pattern at the given point of intersection with the object.
+func PatternAt(pattern Pattern, object Object, worldPoint Tuple) Tuple {
+	inv := object.Transform().Inverse()
+	objectPoint := inv.MultTuple(worldPoint)
+	patternPoint := pattern.Transform().Inverse().MultTuple(objectPoint)
+	return pattern.LocalPatternAt(patternPoint)
 }
