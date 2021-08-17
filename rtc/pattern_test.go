9d5f084f2960560a9f2c3173cbbe6b36c27d182a
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-17 16:17:20 -0400

    Add testPattern

--- rtc/pattern_test.go
+++ rtc/pattern_test.go
@@ -4,10 +4,43 @@ import (
 	"testing"
 )
 
-func TestPatternAt(t *testing.T) {
-	black := Color(0, 0, 0)
-	white := Color(1, 1, 1)
+// testPatternT is a wrapper around a BasePattern for testing purposes.
+// It implements the Pattern interface.
+type testPatternT struct {
+	BasePattern
+}
 
+var _ Pattern = &testPatternT{}
+
+func (t *testPatternT) LocalPatternAt(localPoint Tuple) Tuple {
+	return Color(localPoint.X(), localPoint.Y(), localPoint.Z())
+}
+
+// testPattern creates a test BasePattern. It implements the Object interface.
+func testPattern() *testPatternT {
+	return &testPatternT{
+		BasePattern{transform: M4Identity()},
+	}
+}
+
+func TestPattern_NewTestPattern(t *testing.T) {
+	ts := testPattern()
+	s := ts.BasePattern
+	if !s.Transform().Equal(M4Identity()) {
+		t.Errorf("testPattern default transform should be 4x4 identity matrix, got %v", s.Transform())
+	}
+}
+
+func TestPattern_SetTransform(t *testing.T) {
+	ts := testPattern()
+	s := ts.BasePattern
+	s.SetTransform(Translation(1, 2, 3))
+	if got, want := s.Transform(), Translation(1, 2, 3); !got.Equal(want) {
+		t.Errorf("testPattern setTransform = %v, want %v", s.Transform(), want)
+	}
+}
+
+func TestPatternAt(t *testing.T) {
 	tests := []struct {
 		name             string
 		objectTransform  M4
@@ -16,25 +49,25 @@ func TestPatternAt(t *testing.T) {
 		want             Tuple
 	}{
 		{
-			name:             "Stripes with an object transformation",
+			name:             "A pattern with an object transformation",
 			objectTransform:  Scaling(2, 2, 2),
 			patternTransform: M4Identity(),
-			point:            Point(1.5, 0, 0),
-			want:             white,
+			point:            Point(2, 3, 4),
+			want:             Color(1, 1.5, 2),
 		},
 		{
-			name:             "Stripes with an pattern transformation",
+			name:             "A pattern with an pattern transformation",
 			objectTransform:  M4Identity(),
 			patternTransform: Scaling(2, 2, 2),
-			point:            Point(1.5, 0, 0),
-			want:             white,
+			point:            Point(2, 3, 4),
+			want:             Color(1, 1.5, 2),
 		},
 		{
-			name:             "Stripes with both an object and a pattern transformation",
+			name:             "A pattern with both an object and a pattern transformation",
 			objectTransform:  Scaling(2, 2, 2),
-			patternTransform: Translation(0.5, 0, 0),
-			point:            Point(2.5, 0, 0),
-			want:             white,
+			patternTransform: Translation(0.5, 1, 1.5),
+			point:            Point(2.5, 3, 3.5),
+			want:             Color(0.75, 0.5, 0.25),
 		},
 	}
 
@@ -42,7 +75,7 @@ func TestPatternAt(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			object := Sphere()
 			object.SetTransform(tt.objectTransform)
-			pattern := StripePattern(white, black)
+			pattern := testPattern()
 			pattern.SetTransform(tt.patternTransform)
 
 			if got := PatternAt(pattern, object, tt.point); !got.Equal(tt.want) {
--- rtc/patterns_test.go
+++ rtc/patterns_test.go
@@ -89,3 +89,51 @@ func TestStripePatternT_LocalPatternAt(t *testing.T) {
 		})
 	}
 }
+
+func TestStripePatternAt(t *testing.T) {
+	black := Color(0, 0, 0)
+	white := Color(1, 1, 1)
+
+	tests := []struct {
+		name             string
+		objectTransform  M4
+		patternTransform M4
+		point            Tuple
+		want             Tuple
+	}{
+		{
+			name:             "Stripes with an object transformation",
+			objectTransform:  Scaling(2, 2, 2),
+			patternTransform: M4Identity(),
+			point:            Point(1.5, 0, 0),
+			want:             white,
+		},
+		{
+			name:             "Stripes with an pattern transformation",
+			objectTransform:  M4Identity(),
+			patternTransform: Scaling(2, 2, 2),
+			point:            Point(1.5, 0, 0),
+			want:             white,
+		},
+		{
+			name:             "Stripes with both an object and a pattern transformation",
+			objectTransform:  Scaling(2, 2, 2),
+			patternTransform: Translation(0.5, 0, 0),
+			point:            Point(2.5, 0, 0),
+			want:             white,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			object := Sphere()
+			object.SetTransform(tt.objectTransform)
+			pattern := StripePattern(white, black)
+			pattern.SetTransform(tt.patternTransform)
+
+			if got := PatternAt(pattern, object, tt.point); !got.Equal(tt.want) {
+				t.Errorf("PatternAt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
