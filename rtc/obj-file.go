e719225c9c0b88bd206e78a959670b87b3f73ab5
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-20 16:35:14 -0400

    Modify Wavefront OBJ parser

--- rtc/obj-file.go
+++ rtc/obj-file.go
@@ -28,6 +28,26 @@ func ParseObjFile(filename string) (*ObjFile, error) {
 	return obj, nil
 }
 
+func getFaceIndices(s string) (int, int) {
+	var p, n int
+	parseArgs(s, "/", func(args ...interface{}) {
+		if len(args) != 3 {
+			log.Fatalf("expect 3 parts to face argument, got %#v", args)
+		}
+		p = int(args[0].(float64))
+		n = int(args[2].(float64))
+	})
+	return p, n
+}
+
+func get3Floats(v ...interface{}) (float64, float64, float64) {
+	if len(v) != 3 {
+		log.Fatalf("expect 3 floats, got %#v", v)
+	}
+	x, y, z := v[0].(float64), v[1].(float64), v[2].(float64)
+	return x, y, z
+}
+
 // ParseObj parse a Wavefront OBJ and returns an ObjFile.
 func ParseObj(r io.Reader) (*ObjFile, error) {
 	obj := &ObjFile{
@@ -38,26 +58,35 @@ func ParseObj(r io.Reader) (*ObjFile, error) {
 	}
 	lastGroup := obj.DefaultGroup
 
-	addVertex := func(v ...float64) {
-		if len(v) != 3 {
-			log.Fatalf("expect 3 vertex values, got %#v", v)
-		}
-		obj.Vertices = append(obj.Vertices, Point(v[0], v[1], v[2]))
+	addVertex := func(v ...interface{}) {
+		obj.Vertices = append(obj.Vertices, Point(get3Floats(v...)))
 	}
 
-	addNormal := func(v ...float64) {
-		if len(v) != 3 {
-			log.Fatalf("expect 3 normal values, got %#v", v)
-		}
-		obj.Normals = append(obj.Normals, Vector(v[0], v[1], v[2]))
+	addNormal := func(v ...interface{}) {
+		obj.Normals = append(obj.Normals, Vector(get3Floats(v...)))
 	}
 
-	addTriangle := func(v ...float64) {
+	addTriangle := func(v ...interface{}) {
 		if len(v) < 3 {
-			log.Fatalf("expect 3 or more faces, got %#v", v)
+			log.Fatalf("expect 3 or more face arguments, got %#v", v)
 		}
+		if _, ok := v[0].(float64); ok {
+			p1 := int(v[0].(float64))
+			for i := 2; i < len(v); i++ {
+				p2, p3 := int(v[i-1].(float64)), int(v[i].(float64))
+				lastGroup.AddChild(Triangle(obj.Vertices[p1], obj.Vertices[p2], obj.Vertices[p3]))
+			}
+			return
+		}
+
+		p1, n1 := getFaceIndices(v[0].(string))
 		for i := 2; i < len(v); i++ {
-			lastGroup.AddChild(Triangle(obj.Vertices[int(v[0])], obj.Vertices[int(v[i-1])], obj.Vertices[int(v[i])]))
+			p2, n2 := getFaceIndices(v[i-1].(string))
+			p3, n3 := getFaceIndices(v[i].(string))
+			lastGroup.AddChild(SmoothTriangle(
+				obj.Vertices[p1], obj.Vertices[p2], obj.Vertices[p3],
+				obj.Normals[n1], obj.Normals[n2], obj.Normals[n3],
+			))
 		}
 	}
 
@@ -69,14 +98,15 @@ func ParseObj(r io.Reader) (*ObjFile, error) {
 		}
 
 		line = strings.TrimSpace(line)
+		log.Printf("line=%v", line)
 		switch {
 		case line == "": // silently ignore
 		case strings.HasPrefix(line, "v "):
-			parseTriplet(line[2:], addVertex)
+			parseArgs(line[2:], " ", addVertex)
 		case strings.HasPrefix(line, "vn "):
-			parseTriplet(line[3:], addNormal)
+			parseArgs(line[3:], " ", addNormal)
 		case strings.HasPrefix(line, "f "):
-			parseTriplet(line[2:], addTriangle)
+			parseArgs(line[2:], " ", addTriangle)
 		case strings.HasPrefix(line, "g "):
 			groupName := strings.TrimSpace(line[2:])
 			lastGroup = Group()
@@ -102,17 +132,18 @@ type ObjFile struct {
 	NamedGroups map[string]*GroupT
 }
 
-func parseTriplet(s string, f func(args ...float64)) {
-	parts := strings.Split(strings.TrimSpace(s), " ")
-	var args []float64
+func parseArgs(s, sep string, f func(args ...interface{})) {
+	parts := strings.Split(strings.TrimSpace(s), sep)
+	var args []interface{}
 	for _, arg := range parts {
 		v, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			log.Printf("WARNING: parsing error on line %q, ignored.", s)
-			break
+		if err == nil {
+			args = append(args, v)
+		} else {
+			args = append(args, arg)
 		}
-		args = append(args, v)
 	}
+	log.Printf("args=%#v", args)
 	f(args...)
 }
 
