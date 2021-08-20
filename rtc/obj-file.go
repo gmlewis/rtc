4bfa0cd93602703031c933c706004df3d1206fad
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-20 09:36:20 -0400

    Parse Wavefront OBJ vertices

--- rtc/obj-file.go
+++ rtc/obj-file.go
@@ -3,13 +3,15 @@ package rtc
 import (
 	"bufio"
 	"io"
+	"log"
+	"strconv"
 	"strings"
 )
 
 // ParseObjFile parses a Wavefront OBJ file and returns an Object
 // and the number of lines that were ignored.
 func ParseObjFile(r io.Reader) (*ObjFile, error) {
-	obj := &ObjFile{}
+	obj := &ObjFile{Vertices: []Tuple{Point(0, 0, 0)}} // Vertex 0 is unused.
 
 	b := bufio.NewReader(r)
 	for {
@@ -20,6 +22,7 @@ func ParseObjFile(r io.Reader) (*ObjFile, error) {
 
 		switch {
 		case strings.HasPrefix(line, "v "):
+			obj.Vertices = append(obj.Vertices, parseTuple(line[2:], Point))
 		default:
 			obj.IgnoredLines++
 		}
@@ -37,4 +40,20 @@ type ObjFile struct {
 	IgnoredLines int
 
 	Vertices []Tuple
+}
+
+func atof(s string) float64 {
+	v, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return v
+}
+
+func parseTuple(s string, f func(x, y, z float64) Tuple) Tuple {
+	parts := strings.Split(strings.TrimSpace(s), " ")
+	x := atof(parts[0])
+	y := atof(parts[1])
+	z := atof(parts[2])
+	return f(x, y, z)
 }
