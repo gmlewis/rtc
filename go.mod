53af374d13db05a77c0f94f4ce2be6825e9a978c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-20 21:30:20 -0400

    Switch YAML parser

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/gmlewis/rtc
 go 1.16
 
 require (
-	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/google/go-cmp v0.5.6
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	sigs.k8s.io/yaml v1.2.0
 )
--- rtc/yaml-file.go
+++ rtc/yaml-file.go
@@ -1,11 +1,12 @@
 package rtc
 
 import (
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"os"
 
-	"github.com/go-yaml/yaml"
+	"sigs.k8s.io/yaml"
 )
 
 // ParseYAMLFile parses a yaml scene description file and returns a YAMLFile.
@@ -50,31 +51,37 @@ type YAMLFile struct {
 
 // Item represents anything that can be added to the scene.
 type Item struct {
-	Add    *string `yaml:"add,omitempty"`
-	Define *string `yaml:"define,omitempty"`
+	Add    *string `json:"add,omitempty"`
+	Define *string `json:"define,omitempty"`
 
 	// camera
-	Width  *int      `yaml:"width,omitempty"`
-	Height *int      `yaml:"height,omitempty"`
-	FOV    *float64  `yaml:"field-of-view,omitempty"`
-	From   []float64 `yaml:"from,omitempty"`
-	To     []float64 `yaml:"to,omitempty"`
-	Up     []float64 `yaml:"up,omitempty"`
+	Width  *int      `json:"width,omitempty"`
+	Height *int      `json:"height,omitempty"`
+	FOV    *float64  `json:"field-of-view,omitempty"`
+	From   []float64 `json:"from,omitempty"`
+	To     []float64 `json:"to,omitempty"`
+	Up     []float64 `json:"up,omitempty"`
 
 	// light
-	At        []float64 `yaml:"at,omitempty"`
-	Intensity []float64 `yaml:"intensity,omitempty"`
+	At        []float64 `json:"at,omitempty"`
+	Intensity []float64 `json:"intensity,omitempty"`
 
 	// define
-	Extend *string `yaml:"extend,omitempty"`
-	Value  *Value  `yaml:"value,omitempty"`
+	Extend *string         `json:"extend,omitempty"`
+	Value  json.RawMessage `json:"value,omitempty"`
+
+	// object
+	Material json.RawMessage `json:"material,omitempty"`
 }
 
-// Value represents a value within a "define" section.
-type Value struct {
-	Color      []float64 `yaml:"color,omitempty"`
-	Diffuse    float64   `yaml:"diffuse,omitempty"`
-	Ambient    float64   `yaml:"ambient,omitempty"`
-	Specular   float64   `yaml:"specular,omitempty"`
-	Reflective float64   `yaml:"reflective,omitempty"`
+// YAMLMaterial represents a Material.
+type YAMLMaterial struct {
+	Color           []float64 `json:"color,omitempty"`
+	Diffuse         *float64  `json:"diffuse,omitempty"`
+	Ambient         *float64  `json:"ambient,omitempty"`
+	Specular        *float64  `json:"specular,omitempty"`
+	Sininess        *float64  `json:"shininess,omitempty"`
+	Reflective      *float64  `json:"reflective,omitempty"`
+	Transparency    *float64  `json:"transparency,omitempty"`
+	RefractiveIndex *float64  `json:"refractive-index,omitempty"`
 }
