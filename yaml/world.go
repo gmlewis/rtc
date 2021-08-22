fd20847d096916d9ad602a636344ffe6b7796a44
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-08-21 22:00:20 -0400

    Add missing file

---
+++ yaml/world.go
@@ -0,0 +1,8 @@
+package yaml
+
+import "github.com/gmlewis/rtc/rtc"
+
+// AddToWorld adds the yaml data to the RTC world as is (no added groups).
+func (y *YAMLFile) AddToWorld(w *rtc.WorldT) {
+
+}
